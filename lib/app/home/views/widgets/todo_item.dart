05edbb2b3cd3be07028d819d9f9dcd389dd7ff21
Takudzwa Nyanhanga <takutitus09@gmail.com>
2025-07-19 17:38:54 +0200

    feat: ✨ Add swipe-to-delete in TodoItem widget, enhance user interaction
    refactor: ♻️ Change TodoItem from Stateless to Stateful widget, for maintaining state with new interactions

--- lib/app/home/views/widgets/todo_item.dart
+++ lib/app/home/views/widgets/todo_item.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../../../../utils/constants/dimens.dart';
 
-class TodoItem extends StatelessWidget {
+class TodoItem extends StatefulWidget {
   const TodoItem({
     super.key,
     required this.isCompleted,
@@ -21,47 +22,73 @@ class TodoItem extends StatelessWidget {
   final Function() onEdit;
 
   @override
+  State<TodoItem> createState() => _TodoItemState();
+}
+
+class _TodoItemState extends State<TodoItem>
+    with SingleTickerProviderStateMixin {
+  late final controller = SlidableController(this);
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(
         horizontal: Dimens.padding.small,
         vertical: Dimens.padding.normal,
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Icon(
-            isCompleted
-                ? Icons.check_circle_outline
-                : Icons.radio_button_unchecked,
-            size: 24,
-            color: isCompleted
-                ? Theme.of(context).colorScheme.primary
-                : Colors.grey,
-          ),
-          SizedBox(width: Dimens.spacing.normal),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text('Task', style: Theme.of(context).textTheme.titleLarge),
-                SizedBox(height: Dimens.spacing.small),
-                Text(
-                  'Today, 9:30AM',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
+      child: Slidable(
+        endActionPane: ActionPane(
+          extentRatio: 0.25,
+          motion: const BehindMotion(),
+          children: [
+            SlidableAction(
+              flex: 1,
+              onPressed: (_) {
+                widget.onDelete();
+              },
+              backgroundColor: Colors.red,
+              //foregroundColor: Colors.red,
+              icon: Icons.delete,
+              label: 'Delete',
             ),
-          ),
-          Spacer(),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              widget.isCompleted
+                  ? Icons.check_circle_outline
+                  : Icons.radio_button_unchecked,
+              size: 24,
+              color: widget.isCompleted
+                  ? Theme.of(context).colorScheme.primary
+                  : Colors.grey,
+            ),
+            SizedBox(width: Dimens.spacing.normal),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Task', style: Theme.of(context).textTheme.titleLarge),
+                  SizedBox(height: Dimens.spacing.small),
+                  Text(
+                    'Today, 9:30AM',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+            ),
+            Spacer(),
 
-          IconButton(
-            icon: Icon(Icons.check),
-            color: Colors.green,
-            onPressed: onComplete,
-          ),
-        ],
+            IconButton(
+              icon: Icon(Icons.check),
+              color: Colors.green,
+              onPressed: widget.onComplete,
+            ),
+          ],
+        ),
       ),
     );
   }
